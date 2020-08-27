51c18a0c3315b7852f38849186700fe394bb286f
anderson <andersonrochatavares@gmail.com>
2020-08-27 12:14:52 -0300

    testing on 20-var instances instead of 50, much faster!

--- test/test_dpll_count_satlib.py
+++ test/test_dpll_count_satlib.py
@@ -9,8 +9,39 @@ from pysat.formula import CNF
 from dpll.dpll_count import DPLLCount
 
 
-class TestDPLLCountSatlib(unittest.TestCase):
+class TestDPLLCount(unittest.TestCase):
 
+    def test_count_sat20vars(self):
+        """
+        Tests the counter on 100 SATisfiable random 3CNFinstances with 20 vars
+        (they were generated with cnfgen rather than from SATLIB)
+        :return:
+        """
+        tmp_dir = '/tmp/sat100'
+        with tarfile.open('instances/3cnf_v20_sat.tar.gz') as tf:
+            tf.extractall(tmp_dir)
+        for f in os.listdir(tmp_dir):
+            print(f'Testing {f}')
+            self.assertGreater(DPLLCount(cnf_file=os.path.join(tmp_dir, f)).count(), 0)
+        shutil.rmtree(tmp_dir)
+
+    def test_count_unsat20vars(self):
+        """
+        Tests the solver 100 UNSATisfiable random 3CNFinstances with 20 vars
+        (they were generated with cnfgen rather than from SATLIB)
+        :return:
+        """
+        tmp_dir = '/tmp/unsat100'
+        with tarfile.open('instances/3cnf_v20_unsat.tar.gz') as tf:
+            tf.extractall(tmp_dir)
+        for f in os.listdir(tmp_dir):
+            path = os.path.join(tmp_dir, f)
+            with Solver(bootstrap_with=CNF(path).clauses) as s:
+                count = len(list(s.enum_models()))
+            self.assertEqual(count, DPLLCount(cnf_file=path).count())
+        shutil.rmtree(tmp_dir)
+
+    '''
     def test_dpll_solve_satlib_uf50_01(self):
         """
         Tests the solver on a SATisfiable instance from satlib
@@ -60,7 +91,7 @@ class TestDPLLCountSatlib(unittest.TestCase):
             print(f'Testing {f}')
             self.assertEqual(0, DPLLCount(cnf_file=os.path.join(tmp_dir, f)).count())
         shutil.rmtree(tmp_dir)
-
+    '''
 
 if __name__ == '__main__':
     unittest.main()
